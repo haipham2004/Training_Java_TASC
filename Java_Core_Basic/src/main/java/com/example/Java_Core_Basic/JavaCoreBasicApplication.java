ecd1b26f4b6499723ce5cdc1394719deb12d2da2
haipham2004 <bophamnb2004@gmail.com>
2024-10-14 21:24:05 +0700

    HP321

---
+++ Java_Core_Basic/src/main/java/com/example/Java_Core_Basic/JavaCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_Core_Basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreBasicApplication.class, args);
+	}
+
+}
---
+++ Java_Core_Basic/src/test/java/com/example/Java_Core_Basic/JavaCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_Core_Basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
