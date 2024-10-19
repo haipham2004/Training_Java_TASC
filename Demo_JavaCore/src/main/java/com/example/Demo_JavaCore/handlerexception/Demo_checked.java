74205efa8a39ac394c241eed5aa0b9baaa16a3b6
haipham2004 <bophamnb2004@gmail.com>
2024-10-19 10:03:08 +0700

    update_javacore

--- Demo_JavaCore/src/main/java/com/example/Demo_JavaCore/handlerexception/Demo_checked.java
+++ Demo_JavaCore/src/main/java/com/example/Demo_JavaCore/handlerexception/Demo_checked.java
@@ -1,35 +1,34 @@
 package com.example.Demo_JavaCore.handlerexception;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Demo_checked {
+    // Phương thức ném ra checked exception: throws
+    public static Date displayDate1(String dateString) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("Ngày hợp lệ: "+formatter.parse(dateString));
+        return formatter.parse(dateString);
+    }
 
-    // Phương thức kiểm tra tuổi
-    public static void checkAge(int age) {
-        if (age < 0) {
-            // Ngoại lệ từ custom
-            throw new Demo_Custom_Exception("Tuổi không thể âm!");
-        } else {
-            System.out.println("Tuổi hợp lệ: " + age);
+    // Phương thức xử lý checked exception: try-catch
+    public static void displayDate2(String dateString) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = formatter.parse(dateString);
+            System.out.println("Ngày hợp lệ: " + date);
+        } catch (ParseException e) {
+            System.out.println("Lỗi: Định dạng ngày không hợp lệ - " + e.getMessage());
         }
     }
 
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.print("Nhập tuổi: ");
+    public static void main(String[] args) throws ParseException {
         try {
-            int age = scanner.nextInt();
-            checkAge(age);
-        } catch (InputMismatchException e) {
-            System.out.println("Vui lòng nhập một số nguyên hợp lệ!");
-        } catch (Demo_Custom_Exception e) {
-            System.out.println("Ngoại lệ: " + e.getMessage());
-        } finally {
-            scanner.close(); // Đóng Scanner
+            displayDate1("2024-10-19");  // Hợp lệ
+            displayDate2("19/10/2024");   // Không hợp lệ
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
